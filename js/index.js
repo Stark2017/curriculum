99efa8188e80711b7d425999f47762baae7ca68a
Juan Barrionuevo <juan.barrionuevo2015@outlook.com>
2023-02-22 20:19:58 -0300

    ultimando detalles

--- js/index.js
+++ js/index.js
@@ -1,18 +1,5 @@
 /* 
-Agregar menu hamburguesa fijo, desplegable de izquierda a derecha, puede ocupar un 25% en el desktop, y un 100% de la pantalla en mobile, también el logo hamburguesa estará  visible en todos los sub menú, y en la version mobile. 
-A su vez, puedo dejar fijo por encima del head, un nav con la lista desordenada y un logo que me lleve al inicio. 
-
 Es necesario agregar un loader para la carga inicial del renderizado y navegación entre secciones
 
-Pie de página
-El menú de pie de página es el mismo en la página principal, en las páginas de categorías y en los artículos individuales.
-
-
-Aprovecha tu menú de pie de página
-
-usa el espacio al final de cada página para resaltar el contenido valioso.
-El pie de página te permite resaltar el contenido que «no cabía» en el encabezado.
-
-- homogeneizar la navegacion -
-
 web components  */
+
